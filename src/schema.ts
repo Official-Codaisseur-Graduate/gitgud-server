70c48721a4ddf1960daa0d1bbfc130cc8a1f36ad
Mqspx800 <applefor1988@gmail.com>
2019-09-02 14:56:25 +0200

    separate query into independent file

--- src/schema.ts
+++ src/schema.ts
@@ -4,78 +4,8 @@ import { fetchRepoData } from "./data/repoDetails";
 import { analyzeProfile } from "./data/profileScore";
 import { fetchGeneralData } from "./data/gitUse";
 import { Score } from "./score/entity";
+const typeDefs = require('./gqlQuery').default
 
-const typeDefs = `
-  type Query {
-    user(username: String): User
-    repository(owner: String, name: String): Repository
-  }
-
-  type User {
-    username: String
-    score: Int
-    profileScore: Int
-    averageRepoScore: Int
-    repoScore: Int
-    profileStats: Profile
-    stats: Stats
-    previousScores: [History]
-  }
-
-  type History {
-    id: Int
-    userName: String
-    profileScore: Int
-    gitScore: Int
-    createdAt: String
-  }
-
-  type Profile {
-    bio: Boolean
-    email: Boolean
-    isHireable: Boolean
-    location: Boolean
-    name: Boolean
-    websiteUrl: Boolean
-    pinnedRepositories: Boolean
-    picture: Boolean
-  }
-
-  type Stats {
-    totalPinnedRepos: Int
-    averageBranchPerRepo: Int
-    averageCommitPerBranch: Int
-    repoNames: [Repository]
-  }
-
-  type Repository {
-    name: String
-    owner: String
-    commitScore: Commit
-    branchScore: Branch
-    totalRepoScore: Int
-    repoReadMe: Int
-    gitIgnoreScore: Int
-    description: Int
-  }
-
-  type Commit {
-    lengthExceeds: Int
-    containsAND: Int
-    containsPeriod: Int
-    upperCase: Int
-    totalScore: Int
-  }
-
-  type Branch {
-    hasThreeBranches: Int
-    hasMasterBranch: Int
-    hasDevelopmentBranch: Int
-    hasFeatBranch: Int
-    useDescriptiveNames: Int
-    totalScore: Int
-}
-`;
 
 const resolvers = {
   Query: {
